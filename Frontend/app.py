63aeb276bb24850fe3431d54b30fecd28b53b5b7
bhaskardas1234 <bdas40590@gmail.com>
2023-07-27 13:29:12 -0700

    frontend update

--- Frontend/app.py
+++ Frontend/app.py
@@ -1,10 +1,10 @@
 from flask import Flask,render_template,jsonify,url_for,redirect,request
-#from waitress import serve
+import re
 app =Flask(__name__)
 import requests
 
 
-@app.route('/')
+
 @app.route('/login')
 def login():
     return render_template('login.html')
@@ -13,17 +13,27 @@ def login():
 def success():
     return render_template('ok.html')
 
-@app.route('/',methods=['POST'])
+
+
+
+
+@app.route('/login',methods=['POST'])
 def get_loginvalue():
     name=request.form['username']
     password=request.form['password']
-    loginjasondata={'name':name,'password':password}
+    if re.match('[a-z|A-Z]',name)==None:
+        print("kfjdkf")
+        return render_template('login.html',item_name='ooo lovely')
+    else:
+        loginjasondata={'name':name,'password':password}
     
-    return jsonify(loginjasondata)
-    # res = request.post('http://localhost:5000/josondata', json=loginjasondata)
+    
+        res = requests.post('http://localhost:8080/logindata', json=loginjasondata)
+        print(res.text)
+        return redirect(url_for('success'))
 
-   # print ('response from server:', res.text)
-    #dictFromServer = res.json()
+
+    
 
 
 @app.route('/register')
@@ -39,8 +49,10 @@ def get_valueFromRagisterPage():
     registerjasondata={"name":name,"email":email,'password':password,'phone':phone,'address':address};
     res = requests.post('http://localhost:8080/user', json=registerjasondata)
     print(res.text)
+    p=dict(res.text)
+    print(type(p))
+    print(p)
     return redirect(url_for('success'))
 
-#serve(app,host="0.0.0.0",port=50100)
 if __name__ == "__main__":
     app.run()
